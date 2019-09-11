5cdd25ee66144e39ff61c0bf242c6c9ee334bf09
Aleksandar Andevski <a.andevski@gmail.com>
2019-09-11 11:57:33 +0200

    Remove dev tools
    Causing problems with WebOS

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducer, 
-	compose(applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
-);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
